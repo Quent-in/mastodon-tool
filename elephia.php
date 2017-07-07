c116c98945cc6a296f6c882465172251e4ceffe3
Qwerty <champlywood@free.fr>
2017-07-07 15:47:41 +0200

    Accept RSS feed

--- elephia.php
+++ elephia.php
@@ -1,13 +1,12 @@
 <?php
-// convert Atom feed to Mastodon account
+// convert Atom/RSS feed to Mastodon account
 if(!file_exists('Mastodon_api.php')) {file_put_contents('Mastodon_api.php', file_get_contents('https://raw.githubusercontent.com/yks118/Mastodon-api-php/master/Mastodon_api.php'));} else {include 'Mastodon_api.php';}
 if(!file_exists('config.php')) {file_put_contents('config.php', "<?php
 define('INSTANCE', 'mastodon.social');
 define('EMAIL', 'john.doe@example.org');
 define('PASSWORD', 'mysuperpassword');
-define('ATOM_FEED', 'http://example.org/feed.xml');
-$ban_tags = array();
-");} else {include 'config.php';}
+define('FEED', 'http://example.org/feed.xml');
+define('BAN_TAGS', array());");} else {include 'config.php';}
 if(!file_exists('lastsend.txt')) {file_put_contents('lastsend.txt', 0);}
 
 $mastodon_api = new Mastodon_api();
@@ -17,32 +16,58 @@ $mastodon_api->set_client($create_app['html']['client_id'],$create_app['html']['
 $login = $mastodon_api->login(EMAIL, PASSWORD);
 $mastodon_api->set_token($login['html']['access_token'],$login['html']['token_type']);
 
-print_r($mastodon_api->timelines_home());
+// print_r($mastodon_api->timelines_home());
 
-$array = json_decode(json_encode(simplexml_load_string(implode(file(ATOM_FEED)))), true);
-foreach ($feed->entry as $item) {
-	if(strtotime($item->updated) >= file_get_contents('lastsend.txt')) {
-		$mastodon_api->post_statuses(array('status'=>'[BOT] « '.$item->title.' » '.$item->link['href']));
+if(@preg_match('~<rss(.*)</rss>~si', file_get_contents(FEED))){$type='rss';}
+elseif(@preg_match('~<feed(.*)</feed>~si', file_get_contents(FEED))){$type='atom';}
+else $type = false;
+$feed = preg_replace('/[^\x9\xa\x20-\xD7FF\xE000-\xFFFD]/', '', implode(file(FEED)));
+$array = json_decode(json_encode(simplexml_load_string($feed, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+// print_r($array);
+if($type == 'atom') {
+	foreach ($feed->entry as $item) {
+		if(strtotime($item->updated) >= file_get_contents('lastsend.txt')) {
+			$mastodon_api->post_statuses(array('status'=>'[BOT] « '.$item->title.' » '.$item->link['href']));
+		}
+	}
+	foreach($array['entry'] as $item) {
+		$hashtag = '';
+		foreach($item['category'] as $tags) {
+			if(isset($tags['@attributes'])) {
+				if(!in_array($tags['@attributes']['label'], BAN_TAGS)) {
+					$hashtag .= '#'.$tags['@attributes']['label'].' ';
+				}
+			}
+			else {
+				if(!in_array($tags['label'], BAN_TAGS)) {
+					$hashtag .= '#'.$tags['label'].' ';
+				}
+			}
+		}
+		if(strtotime($item['updated']) >= file_get_contents('lastsend.txt')) { 
+			$mastodon_api->post_statuses(array('status'=>'« '.$item['title'].' » '.$item['link']['@attributes']['href'].' '.$hashtag));
+			echo '« '.$item['title'].' » '.$item['link']['@attributes']['href'].' '.$hashtag.'<br>';
+		}
 	}
 }
-foreach($array['entry'] as $item) {
+elseif($type == 'rss') {
+	// print_r($array['channel']['item']);
 	$hashtag = '';
-	foreach($item['category'] as $tags) {
-		if(isset($tags['@attributes'])) {
-			if(!in_array($tags['@attributes']['label'], $ban_tags)) {
-				$hashtag .= '#'.$tags['@attributes']['label'].' ';
+	foreach ($array['channel']['item'] as $item) {
+		if(isset($item['category'])) {
+			if(is_array($item['category'])) {
+				foreach($item['category'] as $c) {
+					if(!in_array($c, BAN_TAGS)) {$hashtag .= ' #'.$c;}
+				}
+			}
+			else {
+					if(!in_array($tags['category'], BAN_TAGS)) {$hashtag .= ' #'.$item['category'];}
 			}
 		}
-		else {
-			if(!in_array($tags['label'], $ban_tags)) {
-				$hashtag .= '#'.$tags['label'].' ';
-			}
+		
+		if(strtotime($item['pubDate']) >= file_get_contents('lastsend.txt')) {
+			$mastodon_api->post_statuses(array('status'=>'[BOT] « '.$item['title'].' » '.$item['link']));
 		}
-	}
-	if(strtotime($item['updated']) >= file_get_contents('lastsend.txt')) { 
-		$mastodon_api->post_statuses(array('status'=>'« '.$item['title'].' » '.$item['link']['@attributes']['href'].' '.$hashtag));
-		echo '« '.$item['title'].' » '.$item['link']['@attributes']['href'].' '.$hashtag.'<br>';
 	}
 }
-
 file_put_contents('lastsend.txt', time());
